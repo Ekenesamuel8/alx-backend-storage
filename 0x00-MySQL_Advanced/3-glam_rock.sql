c688f4518b07bab488db5f9ae11d793d73f831a3
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-08-08 15:15:58 +0000

    calcuate

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,10 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULLL(split, 2022) - formed) AS lifespan
 -- IFNULL: USE 2022 if split culumn is null
 -- to get the longevity: split minus formed
-FROM metal_bands
 -- from metal_bands table
-WHERE style LIKE '%Glam rock%'
 -- select only glam rock from style culume
+SELECT band_name, (IFNULLL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
+
